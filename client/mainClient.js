92eb453fa0072c8a299e0622881b9085a346576a
Arpit Garg <arpitgarg@Arpits-MacBook-Pro.local>
2014-10-28 00:13:21 +0530

    first commit.. users can submit answers.

---
+++ client/mainClient.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+   
+    Answers = new Meteor.Collection("answers");
+    
+    Template.addAnswer.events({
+    	'click input.add-answer' : function(e){
+    		e.preventDefault();
+    		var answerText = document.getElementById("answerText").value;
+    		Meteor.call("addAnswer",answerText,function(error, answerId){
+    			console.log('Added answer with ID:' + answerId);
+    		});
+    		document.getElementById("answerText").value = "";
+    	}
+    });
+}	
